9571b8a675954da9185bc9e6c8b2cceaa59fee1b
Hugh Macdonald <hugh.macdonald@nvizible.com>
2011-10-31 14:47:37 +0000

    Modified common.py to check the environment variable 'NUKE_EXEC' before defaulting to 'Nuke'

--- nukeExternalControl/common.py
+++ nukeExternalControl/common.py
@@ -1,3 +1,5 @@
+import os
+
 SOCKET_BUFFER_SIZE = 4096
 MAX_SOCKET_BYTES = 2048
 
@@ -7,7 +9,9 @@ DEFAULT_END_PORT = 54300
 
 # This constant should be set to whatever absolute or relative call your system
 # uses to launch Nuke (excluding any flags or arguments).
-NUKE_EXEC = 'Nuke'
+NUKE_EXEC = os.getenv("NUKE_EXEC")
+if NUKE_EXEC is None:
+    NUKE_EXEC = 'Nuke'
 
 # Safe type lists for pickling. Objects whose types are not included in one
 # of these lists will be represented by proxy objects on the client side.
