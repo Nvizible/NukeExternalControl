22eb1b2924ebc8890f53c7e4d4c819ca69d78770
Nathan Rusch <nrusch@github>
2013-01-02 19:17:50 -0800

    Add setup.py for distutils-based package installation

---
+++ setup.py
@@ -0,0 +1,4 @@
+import distutils.core
+import nukeExternalControl
+distutils.core.setup(name='NukeExternalControl',
+                     version=nukeExternalControl.version, packages=['nukeExternalControl'])
